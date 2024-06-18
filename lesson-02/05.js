0d1d8c0ebc9481f11334599574fce1bf13e8fc26
alexanderkalyupanov <138789735+alexanderkalyupanov@users.noreply.github.com>
2024-06-18 13:42:02 +0300

    completed task 2.5

---
+++ lesson-02/05.js
@@ -0,0 +1,14 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passport2 = {};
+
+for (let key in passport) {
+    passport2[key] = passport[key]
+}
+
+passport2.name = "Ivan"
+console.log(passport)
+console.log(passport2)
